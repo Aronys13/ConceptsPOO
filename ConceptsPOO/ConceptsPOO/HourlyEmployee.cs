c988b71cdf7793b319c145b5a074aee928348eec
arony13 <arony@ARONYS>
2023-01-23 11:37:59 -1200

    Finish POO Concepts

---
+++ ConceptsPOO/ConceptsPOO/HourlyEmployee.cs
@@ -0,0 +1,23 @@
+﻿namespace ConceptsPOO
+{
+    public class HourlyEmployee : Employee
+    {
+        public float Hours { get; set; }
+
+        public decimal HourValue { get; set; }
+
+        public override decimal GetValueToPay()
+
+        {
+            return (decimal)Hours * HourValue;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"\n\tHours: {$"{Hours:P2}",15}" +
+                $"\n\tSales................: {$"{HourValue:N2}",15}" +
+                $"\n\tValue to Pay.........: {$"{GetValueToPay():C2}",15}";
+        }
+    }
+}
--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -14,7 +14,7 @@ Employee employee1 = new SalaryEmployee()
     Salary = 1500
 };
 
-Console.WriteLine(employee1);
+//Console.WriteLine(employee1);
 
 Employee employee2 = new CommissionEmployee()
 {
@@ -28,4 +28,69 @@ Employee employee2 = new CommissionEmployee()
 
 };
 
-Console.WriteLine(employee2);
+//Console.WriteLine(employee2);
+
+Employee employee3 = new HourlyEmployee()
+{
+    Id = 2,
+    FirstName = "Natanael",
+    LastName = "Peralta",
+
+    IsActive = true,
+    HourValue = 7500.55M,
+    Hours = 95.05F
+
+};
+
+//Console.WriteLine(employee3);
+
+Employee employee4 = new BaseCommissionEmployee()
+{
+    Id = 2,
+    FirstName = "ARONYS",
+    LastName = "REYES",
+
+    IsActive = true,
+ 
+    Base = 1950.5M,
+    Sales = 35600m,
+    CommissionPercentaje = 0.2F
+};
+
+//Console.WriteLine(employee4);
+
+ICollection<Employee> employees = new List<Employee>()
+{
+    employee1, employee2, employee3, employee4
+};
+
+decimal payroll =0;
+
+foreach (Employee employee in employees)
+{
+    Console.WriteLine(employee);
+    payroll += employee.GetValueToPay();
+}
+ 
+Console.WriteLine("               ==============");
+Console.WriteLine($"TOTAL         {$"{payroll:C2}", 15}");
+
+
+Invoice invoice1 = new Invoice()
+{
+    Id = 1,
+    Description = "IPHONE 13",
+    Price = 30000M,
+    Quantity = 2
+};
+
+Invoice invoice2 = new Invoice()
+{
+    Id = 2,
+    Description = "PROCESADOR RAYCER 5 5000",
+    Price = 9000M,
+    Quantity = 2
+};
+
+Console.WriteLine(invoice1);
+Console.WriteLine(invoice2);
